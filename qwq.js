b9366292479d7d8dc42e9a24b8ca6339236d1421
Edwin Lau <liumingedwin@163.com>
2025-05-18 15:05:36 +0800

    Update from Markdown Editor

---
+++ qwq.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2025-05-18
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/liumingedwin/tamper-scripts
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
